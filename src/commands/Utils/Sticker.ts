be795e3ea15367852f6b48084c869df516dae101
Ronen6999 <101617329+Ronen6999@users.noreply.github.com>
2022-03-25 13:21:29 +0530

    Update Sticker.ts

--- src/commands/Utils/Sticker.ts
+++ src/commands/Utils/Sticker.ts
@@ -80,8 +80,8 @@ export default class Command extends BaseCommand {
 					})();
 					return {
 						categories,
-						pack: pack[1] || "⚡ Here you go ",
-						author: pack[2] || "Yotsuba ⚡",
+						pack: pack[1] || "💕 Here you go ",
+						author: pack[2] || "Elaina 💕",
 						quality,
 						type: StickerTypes[
 							parsedArgs.flags.includes("--crop") ||
