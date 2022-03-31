c43dab25201d22091cae16d5ccafc63263f62e0a
akuma240 <101508091+akuma240@users.noreply.github.com>
2022-03-31 17:57:06 +0530

    Update Hi.ts

--- src/commands/General/Hi.ts
+++ src/commands/General/Hi.ts
@@ -19,7 +19,7 @@ export default class Command extends BaseCommand {
 
 	run = async (M: ISimplifiedMessage): Promise<void> => {
 		const chitoge =
-			"https://c.tenor.com/ldlrOa43OlMAAAPo/majo-no-tabitabi-the-journey-of-elaina.mp4";
+			"https://c.tenor.com/u-JRRAwUdokAAAPo/naruto-shippuden-anime.mp4";
 		return void this.client.sendMessage(
 			M.from,
 			{ url: chitoge },
@@ -27,7 +27,7 @@ export default class Command extends BaseCommand {
 			{
 				quoted: M.WAMessage,
 				mimetype: Mimetype.gif,
-				caption: `Hello there, Elaina Here 💕. Use something from *${this.client.config.prefix}help* list if you want anything. \n`,
+				caption: `In this world, wherever there is light-there are also shadows. use *${this.client.config.prefix}help* list if you want anything. \n`,
 			}
 		);
 	};
