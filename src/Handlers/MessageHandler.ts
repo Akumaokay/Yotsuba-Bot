a787531e380ef1bd959c8f6db617cf050105b9dd
Akumaokay <102905350+Akumaokay@users.noreply.github.com>
2022-04-03 18:50:11 +0530

    Update MessageHandler.ts

--- src/Handlers/MessageHandler.ts
+++ src/Handlers/MessageHandler.ts
@@ -95,7 +95,7 @@ export default class MessageHandler {
 			)}`
 		   );
 			if (!command)
-			return void M.reply( await request.buffer(`https://media.tenor.com/videos/682317a00a1707fa7288373342c7f4cc/mp4`),
+			return void M.reply( await request.buffer(`https://telegra.ph/file/6d0addaf8fb59200ffc0d.mp4`),
                     MessageType.video,
                     Mimetype.gif,
                     undefined,
